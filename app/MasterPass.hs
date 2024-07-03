1cef2e9907c9701de6f686eb723c647722214ecc
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 <thediamond270@gmail.com>
2024-07-03 12:40:48 -0400

    Fixed character c ounter with working stdin

--- app/MasterPass.hs
+++ app/MasterPass.hs
@@ -3,31 +3,38 @@ module MasterPass where
 import System.IO
 import System.Posix.Terminal
 import System.Posix.IO (stdInput)
+import System.Console.ANSI
 
 getUserInput :: IO ()
 getUserInput = do
   oldSettings <- getTerminalAttributes stdInput
   setTerminalAttributes stdInput (configureTerminal oldSettings) Immediately
   putStrLn "Enter your input: "
-  input <- getLimitedInput 128 0
+  input <- getLimitedInput 128 0 ""
   putStrLn ("\n" ++ input)
   setTerminalAttributes stdInput oldSettings Immediately
 
 configureTerminal :: TerminalAttributes -> TerminalAttributes
 configureTerminal attrs = withoutMode (withoutMode attrs ProcessInput) EnableEcho
 
-getLimitedInput :: Int -> Int -> IO String
-getLimitedInput 0 _ = do
+getLimitedInput :: Int -> Int -> String -> IO String
+getLimitedInput 0 _ _ = do
   putStrLn "\nYou've reached the character limit of 128."
   return []
-getLimitedInput n count = do
+getLimitedInput n count input = do
   eof <- isEOF
   if eof
     then return []
     else do
       char <- getChar
-      putStr $ "\rCharacters entered: " ++ show (count + 1)
+      let newInput = input ++ (char:[])
+      cursorUpLine 1
+      clearLine
+      putStr $ "Characters entered: " ++ show (count + 1)
+      cursorDownLine 1
+      clearLine
+      putStr newInput
       hFlush stdout
-      rest <- getLimitedInput (n - 1) (count + 1)
+      rest <- getLimitedInput (n - 1) (count + 1) newInput
       return (char : rest)
 
