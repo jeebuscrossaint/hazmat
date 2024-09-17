52b9c16f0190df4fbe4f585b576a0c95091ce81d
jeebuscrossaint <thediamond270@gmail.com>
2024-09-16 21:41:18 -0400

    will use different build sys

---
+++ master/master.cc
@@ -0,0 +1,6 @@
+#include "master.hh"
+#include "argon2.h"
+
+std::string master_password::hasher(const std::string &input) {
+    
+}
---
+++ master/master.hh
@@ -0,0 +1,15 @@
+#ifndef MASTER_PASSWORD_H
+#define MASTER_PASSWORD_H
+
+#include <string> 
+#include <vector>
+
+class master_password {
+    public:
+    std::string hasher(const std::string& input);
+
+    private:
+    
+};
+
+#endif // MASTER_PASSWORD_H
