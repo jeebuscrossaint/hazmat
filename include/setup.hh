40135517cd17d1de107e747502c8f88d7a5fa28d
jeebuscrossaint <thediamond270@gmail.com>
2024-11-13 13:06:16 -0500

    writing setup functions

--- include/setup.hh
+++ include/setup.hh
@@ -6,22 +6,46 @@
 #include <fstream>
 #include <iostream>
 #include <filesystem>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <cstdlib>
 
 namespace setup {
 
-class Setup {
+class SETUP {
         public:
         // Constructors and Destructors
-        Setup();
-        ~Setup();
+        SETUP();
+        ~SETUP();
 
         // setup function for the first time the program is ran
         void setup();
 
         // csv to hazmat program conversion so that way the user can import their browser entries!
-        void convert();
+        void inportcsv(std::string* csvinput);
+
+        // hazmat to csv conversion so the user can export data if they quit using my lovely program :(
+        void exportcsv();
 
 };
+
+class CSVROW {
+        public:
+                typedef struct {
+                        std::string url;
+                        std::string username;
+                        std::string password;
+                        std::string httpRealm;
+                        std::string formActionOrigin;
+                        std::string guid;
+                        std::string timeCreated;
+                        std::string timeLastUsed;
+                        std::string timePasswordChanged;
+                } Row;
+
+                
+};
+
 
 }
 
--- src/setup.cc
+++ src/setup.cc
@@ -3,14 +3,30 @@
 using namespace setup;
 namespace fs = std::filesystem;
 
-Setup::Setup() {
-    // Empty
+SETUP::SETUP() {
+        // Empty
 }
 
-Setup::~Setup() {
-    // Empty
+SETUP::~SETUP() {
+        // Empty
 }
 
-void Setup::setup() {
+void SETUP::setup() {
+        const char* env_home = std::getenv("$HOME");
+        if (env_home == nullptr) {
+                std::cerr << "You dont have a home directory? What are you, homeless?" << std::endl; // they call me the buffer flusher
+                std::abort;
+        }
+        std::string hazmatdir = std::string(env_home) + "/.hazmat"; 
+        fs::create_directory(hazmatdir);
+};
+
+void SETUP::inportcsv(std::string* csvinput) {
         
-}
+
+};
+
+void SETUP::exportcsv() {
+
+
+};
