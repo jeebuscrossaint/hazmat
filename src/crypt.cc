680854c5812c083cc957db8b0b7c0e125bb6e0f4
jeebuscrossaint <thediamond270@gmail.com>
2024-11-18 13:12:35 -0500

    verbosity and it builds

--- src/crypt.cc
+++ src/crypt.cc
@@ -2,14 +2,32 @@
 
 using namespace crypto;
 
+cryptosys::cryptosys() {};
+
+cryptosys::~cryptosys() {};
+
+std::string cryptosys::generateSalt() {
+    // Implement salt generation
+    return ""; // Temporary
+}
+
+std::string cryptosys::hashMasterPassword(const std::string& password) {
+    // Implement password hashing
+    return ""; // Temporary
+}
+
+std::string cryptosys::base64Encode(const std::string& input) {
+    // Implement base64 encoding
+    return ""; // Temporary
+}
+
 std::string cryptosys::getSecureInput(const std::string& prompt) {
         std::string password;
-        struct termius old_settings, new_settings;
+        struct termios old_settings, new_settings;
 
         // get current term settings
         tcgetattr(STDIN_FILENO, &old_settings);
-        new_settings = old_settings
-
+        new_settings = old_settings;
         // disable echo!
         new_settings.c_lflag &= ~ECHO;
 
