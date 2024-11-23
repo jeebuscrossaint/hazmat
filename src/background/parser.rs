4b23fa20de617a047757121eadb7bf020a0f2dec
jeebuscrossaint <thediamond270@gmail.com>
2024-11-22 21:20:15 -0500

    wtf

--- src/background/parser.rs
+++
@@ -1,9 +0,0 @@
-use std::{error::Error, io, process};
-
-pub fn import -> Result<(), Box<dyn Error>> {
-	// build csv reader and iterate over each record
-	let mut rdr = csv::Reader::from_reader(io::stdin());
-	for result in rdr.records() {
-		let record = result?;
-	}
-}
