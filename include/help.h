012b899c30091c3aeb63c84770e5cea6ff26eb00
jeebuscrossaint <thediamond270@gmail.com>
2025-04-13 11:29:00 -0400

    nuclear i hate this sh

--- include/help.h
+++
@@ -1,8 +0,0 @@
-#ifndef HELP_H
-#define HELP_H
-
-#include <stdio.h>
-
-void hhelp();
-
-#endif // HELP_H
--- include/random.h
+++
@@ -1,9 +0,0 @@
-#ifndef RANDOM_H
-#define RANDOM_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
-char* genrand(int len);
-
-#endif // RANDOM_H
